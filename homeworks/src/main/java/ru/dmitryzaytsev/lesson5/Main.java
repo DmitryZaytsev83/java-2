67e00a1a22d729111fa1a664d69d54bd663553ba
Dmitry <dmitry.zaytsev83@gmail.com>
2021-03-05 07:45:53 +0300

    fix wrong calculating

--- homeworks/src/main/java/ru/dmitryzaytsev/lesson5/Main.java
+++ homeworks/src/main/java/ru/dmitryzaytsev/lesson5/Main.java
@@ -20,8 +20,8 @@ public class Main {
         Arrays.fill(arr, 1f);
         long start = System.currentTimeMillis();
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) *
-                    Math.cos(0.4f + i / 2));
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + (float) i / 5) * Math.cos(0.2f + (float) i / 5) *
+                    Math.cos(0.4f + (float) i / 2));
         }
         long finish = System.currentTimeMillis();
         System.out.println(finish - start);
@@ -37,14 +37,14 @@ public class Main {
         System.arraycopy(arr, HALF, a2, 0, HALF);
         Thread t1 = new Thread(() -> {
             for (int i = 0; i < a1.length; i++) {
-                a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) *
-                        Math.cos(0.4f + i / 2));
+                a1[i] = (float) (a1[i] * Math.sin(0.2f + (float) i / 5) * Math.cos(0.2f + (float) i / 5) *
+                        Math.cos(0.4f + (float) i / 2));
             }
         });
         Thread t2 = new Thread(() -> {
             for (int i = 0; i < a2.length; i++) {
-                a2[i] = (float) (a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) *
-                        Math.cos(0.4f + i / 2));
+                a2[i] = (float) (a2[i] * Math.sin(0.2f + (float) (i + HALF) / 5) * Math.cos(0.2f + (float) (i + HALF) / 5) *
+                        Math.cos(0.4f + (float) (i + HALF) / 2));
             }
         });
         t1.start();
